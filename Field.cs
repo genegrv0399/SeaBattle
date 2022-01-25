2e9b02058e6368f22a797f2cf437e5e2052868c1
granat319 <69637057+genegrv0399@users.noreply.github.com>
2022-01-26 01:32:20 +0300

    1. Свойства надо доделать по классам 2. В WPF кнопка для примера, по факту надо создать коллекцию программно и передавть в item

---
+++ Field.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle
+{
+    internal class Field
+    {
+        #region Поля
+        private string сondition;
+        private int numberField;
+        private enum ListСondition
+        {
+            // поле занято, поле не занято
+            fieldOccupied, fieldNotOccupied
+        }
+        #endregion
+
+        #region Свойства
+        public string Condition { get; set; }
+        #endregion
+
+        #region Конуструкторы
+        public Field(int k)
+        {
+            this.numberField = k;
+            this.сondition = ListСondition.fieldNotOccupied.ToString();
+        }
+        #endregion
+    }
+}
---
+++ Ship.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle
+{
+  
+
+    //Корабль
+    class Ship
+    {
+        #region Перечисления
+        private enum ListNameShip
+        {
+            //катер - однопалубник, эсминец - двухпалубник, крейсер - трёхпалубник, линкор - четрыёхпалубник    
+            Boat, Destroyer, Cruiser, Battleship
+        }
+        private enum ListСondition
+        {
+            //не пробит, пробит, уничтожен
+            notBroken, broken, destroyed
+        }
+        #endregion
+
+        #region Поля
+        private readonly string name;
+        private int numberOfDecks;
+        private string condition;
+        List<Deck> decks;
+     
+        #endregion
+
+        #region Свойства
+        public string Name { get { return name; } }
+        public int NumberOfDecks { get { return numberOfDecks; } set { numberOfDecks = value; } }
+        #endregion
+
+        #region Консутрукторы
+        public Ship(int n)
+        {
+            switch (n)
+            {
+                case 1: this.name = ListNameShip.Boat.ToString(); break;
+                case 2: this.name = ListNameShip.Destroyer.ToString(); break;
+                case 3: this.name = ListNameShip.Cruiser.ToString(); break;
+                case 4: this.name = ListNameShip.Battleship.ToString(); break;
+            }
+            this.decks = new List<Deck>();
+            for(int k = 0; k < n; k++)
+            {
+                decks.Add(new Deck(k));
+            }
+            this.condition = ListСondition.notBroken.ToString();
+        }      
+        #endregion
+
+
+        //Палуба
+        class Deck
+        {
+            #region Поля
+            private string conditionDeck;
+            private int deckNumber;
+            #endregion
+
+            #region Конструкторы
+            public Deck(int k)
+            {
+                this.deckNumber = k;
+                this.conditionDeck = ListСondition.notBroken.ToString();
+            }
+            #endregion
+        }
+    }
+
+    
+}
